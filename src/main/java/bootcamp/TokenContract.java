329ec52332a3940d39988bf9c09838776055bc37
JimChen <jimyjchen@aliyun.com>
2020-10-04 08:29:49 +0800

    first commit

---
+++ src/main/java/bootcamp/TokenContract.java
@@ -0,0 +1,31 @@
+package bootcamp;
+
+import net.corda.core.contracts.CommandData;
+import net.corda.core.contracts.CommandWithParties;
+import net.corda.core.contracts.Contract;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.transactions.LedgerTransaction;
+import java.util.List;
+
+import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;
+import static net.corda.core.contracts.ContractsDSL.requireThat;
+
+
+/* Our contract, governing how our state will evolve over time.
+ * See src/main/java/examples/ArtContract.java for an example. */
+public class TokenContract implements Contract {
+
+    @Override
+    public void verify(LedgerTransaction tx) throws IllegalArgumentException {
+
+    }
+
+    public interface Commands extends CommandData {
+        class Issue implements Commands { }
+        class TransferAsset implements Commands { }
+
+        //class Move implements Commands { }
+        //class Transfer implements Commands{}
+
+    }
+}
